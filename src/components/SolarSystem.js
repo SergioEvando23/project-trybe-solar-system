066cda8c61e82c970fcd380d3614b8bcbd60bc40
SergioEvando23 <sergioevandocosta@gmail.com>
2022-01-25 17:48:54 -0300

    Renderizado o componente Title dentro do componente SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <p>
-          SolarSystem
-        </p>
+        <Title headline="planetas" />
       </div>
     );
   }
