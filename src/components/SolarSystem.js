2b7e0e4ad2e24430fd381c42f526cae050346bfd
SergioEvando23 <sergioevandocosta@gmail.com>
2022-01-25 15:32:21 -0300

    Criado component SolarSystem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        testando solar system
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import SolarSystem from './SolarSystem';
 
 class Index extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SolarSystem />
+      </>
     );
   }
 }
