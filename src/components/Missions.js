bc8d913a1d8ec893dc6be8349fde96dc9c7a47a6
SergioEvando23 <sergioevandocosta@gmail.com>
2022-01-27 13:53:00 -0300

    criado o component Mission Card

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import moduleMissons from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <MissionCard
+          name={ moduleMissons[0].name }
+          year={ moduleMissons[0].year }
+          country={ moduleMissons[0].country }
+          destination={ moduleMissons[0].destination }
+        />
       </div>
     );
   }
